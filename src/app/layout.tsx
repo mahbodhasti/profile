a8fc787b38c07a8a6555e433eef8b7060acc5d54
mahbodhasti <84708375+mahbodhasti@users.noreply.github.com>
2025-09-22 19:35:10 +0330

    Added google-site-verification meta tag in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,14 @@ export const metadata = {
   title: "lor_of_programme",
   description: "وبسایت مهبد هستی",
   verification: {
-    google: "RY6NrlqGQntGaA4V2b3ivViB5cYg4KfLX-9bahNmKc4",
+    google: "RY6NrlqGQntGaA4V2b3ivViB5cYg4KfLX-9bahNmKc4", // همین توکن واقعی
   },
   icons: {
     icon: "https://res.cloudinary.com/dhff7ulyr/image/upload/v1756405118/mahbodlg_qljvds.png",
   },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fa">
       <body className={inter.className}>
